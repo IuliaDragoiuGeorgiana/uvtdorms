7b7830da62771563d411cdb91c048c892c8c2a2d
IuliaDragoiuGeorgiana <iuliadragoiu2@gmail.com>
2024-02-07 16:52:24 +0200

    Refactorized dryer controller and service.

--- backend/src/main/java/com/uvtdorms/controller/DryerController.java
+++ backend/src/main/java/com/uvtdorms/controller/DryerController.java
@@ -1,33 +1,23 @@
 package com.uvtdorms.controller;
 
-import com.uvtdorms.repository.dto.response.DryerDto;
 import com.uvtdorms.services.DryerService;
+
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/api/dryers")
+@RequiredArgsConstructor
 public class DryerController {
     private final DryerService dryerService;
 
-    public DryerController(DryerService dryerService) {
-        this.dryerService = dryerService;
-    }
-
     @GetMapping("/get-dryer-from-dorm/{dormId}")
-    public ResponseEntity<?> getDyerFromDorm(@PathVariable("dormId") String dormId){
-
-        try {
-            List<DryerDto> dryersDtos =  this.dryerService.getDryerFromDorm(dormId);
-            return ResponseEntity.ok(dryersDtos);
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
-
+    public ResponseEntity<?> getDyerFromDorm(@PathVariable("dormId") String dormId) {
+        return ResponseEntity.ok(this.dryerService.getDryerFromDorm(dormId));
     }
 }
--- backend/src/main/java/com/uvtdorms/services/DryerService.java
+++ backend/src/main/java/com/uvtdorms/services/DryerService.java
@@ -1,7 +1,6 @@
 package com.uvtdorms.services;
 
-import com.uvtdorms.exception.DormNotFoundException;
-import com.uvtdorms.exception.WrongUuidException;
+import com.uvtdorms.exception.AppException;
 import com.uvtdorms.repository.IDormRepository;
 import com.uvtdorms.repository.IDryerRepository;
 import com.uvtdorms.repository.dto.response.DryerDto;
@@ -9,45 +8,37 @@ import com.uvtdorms.repository.entity.Dorm;
 import com.uvtdorms.repository.entity.Dryer;
 import com.uvtdorms.services.interfaces.IDryerService;
 
+import lombok.RequiredArgsConstructor;
+
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class DryerService implements IDryerService {
 
     private final IDormRepository dormRepository;
     private final IDryerRepository dryerRepository;
     private final ModelMapper modelMapper;
 
-
-    public DryerService(
-            IDormRepository dormRepository,
-            IDryerRepository dryerRepository,
-            ModelMapper modelMapper)
-    {
-        this.dormRepository = dormRepository;
-        this.dryerRepository = dryerRepository;
-        this.modelMapper = modelMapper;
-    }
-
     @Override
-    public List<DryerDto> getDryerFromDorm(String dormId) throws Exception {
+    public List<DryerDto> getDryerFromDorm(String dormId) throws AppException {
         UUID dormUuid = UUID.fromString(dormId);
 
-        if(dormUuid == null)
-            throw new WrongUuidException();
-
-        Optional<Dorm> dorm = dormRepository.findById(dormUuid);
-        if(dorm.isEmpty()){
-            throw new DormNotFoundException();
+        if (dormUuid == null) {
+            throw new AppException("Invalid UUID", HttpStatus.BAD_REQUEST);
         }
 
-        List<Dryer> dryers = dryerRepository.findByDorm(dorm.get());
+        Dorm dorm = dormRepository.findById(dormUuid)
+                .orElseThrow(() -> new AppException("Dorm not found", HttpStatus.NOT_FOUND));
+
+        List<Dryer> dryers = dryerRepository.findByDorm(dorm);
+
         return dryers.stream()
                 .map(dryer -> modelMapper.map(dryer, DryerDto.class))
                 .collect(Collectors.toList());
